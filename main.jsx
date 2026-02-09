bff3eed5f29a6e801766f24516e732000506575a
Raone Mota <raonemota@hotmail.com>
2026-02-09 11:08:02 -0300

    refactor: Simplify React root setup
    Remove redundant root element check and conditional rendering. The `ReactDOM.createRoot` call is now direct, assuming the 'root' element always exists in the DOM.

--- main.jsx
+++ main.jsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-} else {
-  console.error('Failed to find the root element')
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
